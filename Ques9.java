baaf240b6aac8fe379aba0842280de104507ed7d
Simran Kaur Tiwana <tiwanasimran010@gmail.com>
2025-09-02 13:44:43 +0530

    initial commit

---
+++ Ques9.java
@@ -0,0 +1,9 @@
+public class Ques9 {
+    public static void main(String[] args) {
+        int a = 9;
+        Integer obj = a;  
+        int b = obj;      
+        System.out.println("Autoboxing: " + obj);
+        System.out.println("Unboxing: " + b);
+    }
+}
