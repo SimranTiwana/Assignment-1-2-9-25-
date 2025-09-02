8751be45a0e0bdd941e061af38e0e1d1f6fb6a15
Simran Kaur Tiwana <tiwanasimran010@gmail.com>
2025-09-02 16:12:25 +0530

    initial commit

---
+++ Ques18.java
@@ -0,0 +1,12 @@
+public class Ques18 {
+    public static void main(String[] args) {
+        int maxInt = Integer.MAX_VALUE;
+        System.out.println("Maximum int value: " + maxInt);
+        System.out.println("Adding 1 to maximum int: " + (maxInt + 1));
+        
+        byte maxByte = Byte.MAX_VALUE;
+        System.out.println("Maximum byte value: " + maxByte);
+        System.out.println("Adding 1 to maximum byte: " + (byte)(maxByte + 1));
+       
+    }
+}
