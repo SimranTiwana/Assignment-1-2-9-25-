cdda829ec2c05600cd6ec5a5efd8fb432b61ff58
Simran Kaur Tiwana <tiwanasimran010@gmail.com>
2025-09-02 16:09:35 +0530

    initial commit

---
+++ Ques17.java
@@ -0,0 +1,13 @@
+public class Ques17 {
+    public static void main(String[] args) {
+        var name = "Christopher Alvaro";
+        var age = 24;
+        var height = 1.80;
+        var isStudent = false;
+        
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Is a Student: " + isStudent);
+    }
+}
