cc75a1ff284944e6a1a4e543e8e5c03a0ab6d28d
Simran Kaur Tiwana <tiwanasimran010@gmail.com>
2025-09-02 13:52:01 +0530

    initial commit

---
+++ Ques12.java
@@ -0,0 +1,13 @@
+public class Ques12 {
+    public static void main(String[] args) {
+        byte b = 5;
+        int i = 10;
+        int result1 = b + i;
+        System.out.println("byte + int = " + result1);
+
+        float f = 13.4f;
+        long l = 495L;
+        float result2 = f + l;
+        System.out.println("float + long = " + result2);
+    }
+}
