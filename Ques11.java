6f161b0bb4c6e3176e37a2c86fc46a0f2c48dd92
Simran Kaur Tiwana <tiwanasimran010@gmail.com>
2025-09-02 13:50:10 +0530

    initial commit

---
+++ Ques11.java
@@ -0,0 +1,8 @@
+public class Ques11 {
+    public static void main(String[] args) {
+        char ch = 'S';
+        String str = "Chitkara University";
+        System.out.println("Character datatype : " + ch);
+        System.out.println("String datatype: " + str);
+    }
+}
