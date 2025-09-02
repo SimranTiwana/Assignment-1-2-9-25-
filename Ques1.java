eb701aa6c9f5f0a4b79797a9fd55fceafb155bd2
Simran Kaur Tiwana <tiwanasimran010@gmail.com>
2025-09-02 13:13:39 +0530

    initial commit

---
+++ Ques1.java
@@ -0,0 +1,21 @@
+public class Ques1 {
+    public static void main(String[] args) {
+    byte b = 5;
+    short s = 200;
+    int i = 1500;
+    long l = 5000L;
+    float f = 3.5f;
+    double d = 7.25;
+    char c = 'M';
+    boolean bool = true;
+        
+        System.out.println("byte: " + b);
+        System.out.println("short: " + s);
+        System.out.println("int: " + i);
+        System.out.println("long: " + l);
+        System.out.println("float: " + f);
+        System.out.println("double: " + d);
+        System.out.println("char: " + c);
+        System.out.println("boolean: " + bool);
+    }
+}
