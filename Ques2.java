12e231a34d92715051fc56e7903c67df007e7d1f
Simran Kaur Tiwana <tiwanasimran010@gmail.com>
2025-09-02 13:18:07 +0530

    initial commit

---
+++ Ques2.java
@@ -0,0 +1,12 @@
+public class Ques2{
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 9;
+
+        int sum = num1 + num2;
+
+        System.out.println("Value of First number: " + num1);
+        System.out.println("Value of Second number: " + num2);
+        System.out.println("Total Sum: " + sum);
+    }
+}
